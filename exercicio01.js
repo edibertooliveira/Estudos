007acf5a0185057ffdacf282fe85a497d5e53280
Brenda N <118312482+BrendaSanttana@users.noreply.github.com>
2022-11-15 21:32:51 +0000

    adicionado exercicios de variaveis

---
+++ exercicio01.js
@@ -0,0 +1,7 @@
+const myName = "Brenda"; 
+const birthCity = "Salvador";
+let birthYear = 1996;
+birthYear = 2030
+birthCity = "Rio de Janeiro"
+console.log(myName, birthCity, birthYear);
+console.log(birthYear)
