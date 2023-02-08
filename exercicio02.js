9752d4ba70e51a1e62db268de0c06839d28499f0
Brenda Santana <brenda.nsantana@outlook.com>
2023-02-07 23:06:15 -0300

    removendo do repositorio e estudos

--- exercicio02.js
+++
@@ -1,8 +0,0 @@
-const base = 5;
-const heigth = 8;
-const area = base+heigth;
-const perimeter = base+heigth+area;
-console.log(base);
-console.log(heigth);
-console.log (area);
-console.log (perimeter);
